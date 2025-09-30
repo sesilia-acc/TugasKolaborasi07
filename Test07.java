617092282d64a619b3c70e2736ff4e52aef89c02
reyherfian <rey.adriano.m@gmail.com>
2025-09-30 21:00:32 +0700

    Kolaborasi Aqila

--- Test07.java
+++ Test07.java
@@ -1,5 +1,6 @@
 public class Test07 {
     public static void main(String[] args) {
         System.out.println("Hi! Ini baris kode pertama");
+        System.out.println("Hi! Ini kolaborasi Aqila");
     }
 }
